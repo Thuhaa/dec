761f5191c51d272fdefd007e5eef8a1660dd1f8a
Thuhaa <josephthuha@gmail.com>
2020-12-16 14:56:51 +0300

    Edited the secret key config

--- dec/settings.py
+++ dec/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY=str(os.environ.get('SECRET_KEY_DEC'))
+SECRET_KEY=os.environ.get('SECRET_KEY_DEC')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['immense-hollows-34332.herokuapp.com']
+ALLOWED_HOSTS = ['decapp.herokuapp.com']
 
 
 # Application definition
