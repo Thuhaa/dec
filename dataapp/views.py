0e3a3c5a8a6b9dafcbf1ebfd2a60a83d33d8192c
Thuhaa <josephthuha@gmail.com>
2020-12-18 12:14:31 +0300

    Started working on my decapp application

--- dataapp/views.py
+++ dataapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepageview(request):
-	return HttpResponse("This will be the homepage")
+	return render(request, 'dataapp/homepage.html')
 # Create your views here.
